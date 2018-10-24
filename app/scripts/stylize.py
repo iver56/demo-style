7d7548eed3cbb27b24b4ad5289e099e4d7ccc600
iver56 <ij@aiascience.com>
2018-10-24 14:16:20 +0200

    Add overwrite option. Reformat code with black.

--- app/scripts/stylize.py
+++ app/scripts/stylize.py
@@ -15,11 +15,11 @@ def get_label_filename(filename):
 
     This label file shows the semantic segmentation for the image.
     """
-    filename_without_extension = '.'.join(filename.split('.')[:-1])
-    return filename_without_extension + '.__label__.png'
+    filename_without_extension = ".".join(filename.split(".")[:-1])
+    return filename_without_extension + ".__label__.png"
 
 
-def run(content_image_dir, style_image_dir, host_name):
+def run(content_image_dir, style_image_dir, host_name, overwrite):
     """Iterate over the images in the two folders, and find all combinations of content+style
 
     Generate a stylized image for each combination, and store the result.
@@ -27,22 +27,30 @@ def run(content_image_dir, style_image_dir, host_name):
     Because FastPhotoStyle is hard to set up (it needs a very specific environment),
     it runs on its own server, and the service is exposed as a REST API.
     """
-    rest_api_url = 'http://{}:5000/stylize/'.format(host_name)
+    rest_api_url = "http://{}:5000/stylize/".format(host_name)
 
     content_filenames_iterator = next(os.walk(content_image_dir))[2]
     content_filenames = []
     for filename in content_filenames_iterator:
-        if '__label__' in filename:
+        if "__label__" in filename:
             continue
-        if filename.lower().endswith('.jpg') or filename.lower().endswith('.png') or filename.lower().endswith('.jpeg'):
+        if (
+            filename.lower().endswith(".jpg")
+            or filename.lower().endswith(".png")
+            or filename.lower().endswith(".jpeg")
+        ):
             content_filenames.append(filename)
 
     style_filenames_iterator = next(os.walk(style_image_dir))[2]
     style_filenames = []
     for filename in style_filenames_iterator:
-        if '__label__' in filename:
+        if "__label__" in filename:
             continue
-        if filename.lower().endswith('.jpg') or filename.lower().endswith('.png') or filename.lower().endswith('.jpeg'):
+        if (
+            filename.lower().endswith(".jpg")
+            or filename.lower().endswith(".png")
+            or filename.lower().endswith(".jpeg")
+        ):
             style_filenames.append(filename)
 
     for content_filename in tqdm(content_filenames):
@@ -55,54 +63,80 @@ def run(content_image_dir, style_image_dir, host_name):
             content_label_filename = get_label_filename(content_filename)
             style_label_filename = get_label_filename(style_filename)
             output_image_file_path = os.path.join(
-                STYLIZED_IMAGE_DIR,
-                content_filename + '__' + style_filename + '.png'
+                STYLIZED_IMAGE_DIR, content_filename + "__" + style_filename + ".png"
             )
-            if os.path.isfile(output_image_file_path):
-                print('Skipping, because {} already exists'.format(output_image_file_path))
+            if os.path.isfile(output_image_file_path) and not overwrite:
+                print(
+                    "Skipping, because {} already exists".format(output_image_file_path)
+                )
                 continue
 
-            content_image = Image.open(os.path.join(content_image_dir, content_filename))
+            content_image = Image.open(
+                os.path.join(content_image_dir, content_filename)
+            )
             style_image = Image.open(os.path.join(style_image_dir, style_filename))
 
             content_segmentation_image = None
             style_segmentation_image = None
             try:
-                content_segmentation_image = Image.open(os.path.join(content_image_dir, content_label_filename))
-                style_segmentation_image = Image.open(os.path.join(style_image_dir, style_label_filename))
+                content_segmentation_image = Image.open(
+                    os.path.join(content_image_dir, content_label_filename)
+                )
+                style_segmentation_image = Image.open(
+                    os.path.join(style_image_dir, style_label_filename)
+                )
             except FileNotFoundError as e:
                 print(e)
                 pass
 
             data = {
-                'content_image_base64': pil_image_to_base64(content_image),
-                'content_segmentation_base64': pil_image_to_base64(content_segmentation_image),
-                'style_image_base64': pil_image_to_base64(style_image),
-                'style_segmentation_base64': pil_image_to_base64(style_segmentation_image),
+                "content_image_base64": pil_image_to_base64(content_image),
+                "content_segmentation_base64": pil_image_to_base64(
+                    content_segmentation_image
+                ),
+                "style_image_base64": pil_image_to_base64(style_image),
+                "style_segmentation_base64": pil_image_to_base64(
+                    style_segmentation_image
+                ),
             }
 
             r = requests.post(rest_api_url, json=data, timeout=60)
             json_data = r.json()
-            output_image = base64_png_image_to_pillow_image(json_data['output_image_base64'])
-            output_image.save(
-                output_image_file_path,
-                format='PNG',
+            output_image = base64_png_image_to_pillow_image(
+                json_data["output_image_base64"]
             )
-            print('Stylized one image ({} s)'.format(time.time() - start_time))
+            output_image.save(output_image_file_path, format="PNG")
+            print("Stylized one image ({} s)".format(time.time() - start_time))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
 
     arg_parser.add_argument(
-        '--host',
-        dest='host',
-        help='Specify the IP address or host name of the FastPhotoStyle server.',
+        "--host",
+        dest="host",
+        help="Specify the IP address or host name of the FastPhotoStyle server.",
         type=str,
         required=False,
-        default="localhost"
+        default="localhost",
+    )
+
+    arg_parser.add_argument(
+        "--overwrite",
+        dest="overwrite",
+        help="By default, existing files are not overwritten. This option enables"
+        " overwriting.",
+        nargs="?",
+        const=True,
+        required=False,
+        default=False,
     )
 
     args = arg_parser.parse_args()
 
-    run(content_image_dir=CONTENT_IMAGE_DIR, style_image_dir=STYLE_IMAGE_DIR, host_name=args.host)
+    run(
+        content_image_dir=CONTENT_IMAGE_DIR,
+        style_image_dir=STYLE_IMAGE_DIR,
+        host_name=args.host,
+        overwrite=args.overwrite,
+    )
