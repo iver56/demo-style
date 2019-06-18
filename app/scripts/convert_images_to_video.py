9c87f711f721bd3badfbedc3bc1c158812ec43e4
iver56 <iver56@hotmail.com>
2019-06-18 21:53:09 +0200

    Allow specifying --input-path when running convert_images_to_video.py

--- app/scripts/convert_images_to_video.py
+++ app/scripts/convert_images_to_video.py
@@ -1,41 +1,55 @@
 import argparse
 import os
-from pathlib import Path
 import subprocess
+from pathlib import Path
 
-from app.settings import STYLIZED_IMAGE_DIR, TMP_DIR, VIDEO_OUTPUT_DIR
+from app.settings import STYLIZED_IMAGE_DIR, TMP_DIR
 from app.utils.image import get_image_file_paths
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
 
     arg_parser.add_argument(
-        '--framerate',
-        dest='framerate',
-        help='Specify the framerate as a positive integer',
-        type=int,
+        "--input-path",
+        dest="input_path",
+        help="Path to the folder where the images frames) reside",
+        type=str,
         required=False,
-        default=30
+        default=STYLIZED_IMAGE_DIR,
     )
 
+    arg_parser.add_argument(
+        "--framerate",
+        dest="framerate",
+        help="Specify the framerate as a positive integer",
+        type=int,
+        required=False,
+        default=30,
+    )
     args = arg_parser.parse_args()
 
-    image_file_paths = get_image_file_paths(STYLIZED_IMAGE_DIR)
-    image_list_file_path = os.path.join(TMP_DIR, 'images.txt')
+    image_file_paths = get_image_file_paths(args.input_path)
+    image_list_file_path = os.path.join(TMP_DIR, "images.txt")
 
-    with open(image_list_file_path, 'w') as f:
+    with open(image_list_file_path, "w") as f:
         for path in image_file_paths:
-            f.write('file {}\n'.format(Path(path).as_posix()))
+            f.write("file {}\n".format(Path(path).as_posix()))
 
     subprocess.run(
         [
             "ffmpeg",
-            "-r", "{}".format(args.framerate),
-            "-f", "concat",
-            "-safe", "0",
-            "-i", image_list_file_path,
-            "-c:v", "libx264",
-            "-pix_fmt", "yuv420p",
-            "{}".format(Path(os.path.join(VIDEO_OUTPUT_DIR, "video.mp4")).as_posix())
-        ],
+            "-r",
+            "{}".format(args.framerate),
+            "-f",
+            "concat",
+            "-safe",
+            "0",
+            "-i",
+            image_list_file_path,
+            "-c:v",
+            "libx264",
+            "-pix_fmt",
+            "yuv420p",
+            "{}".format(Path(os.path.join(args.input_path, "video.mp4")).as_posix()),
+        ]
     )
